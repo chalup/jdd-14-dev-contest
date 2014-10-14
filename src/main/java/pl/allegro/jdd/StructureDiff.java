4a689f037d9e41f4948b316b9bdad607be2ce6d7
Jerzy Chalupski <jerzy@getbase.com>
2014-10-14 09:17:43 +0200

    calculate implementation

--- src/main/java/pl/allegro/jdd/StructureDiff.java
+++ src/main/java/pl/allegro/jdd/StructureDiff.java
@@ -2,12 +2,42 @@ package pl.allegro.jdd;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import com.google.common.collect.TreeTraverser;
+
+import java.util.function.Function;
+
 public class StructureDiff {
-    public Changes calculate(Employee oldCTO, Employee newCTO){
+    public Changes calculate(Employee oldCTO, Employee newCTO) {
         checkNotNull(oldCTO);
         checkNotNull(newCTO);
 
-        // your code goes here
-        throw new UnsupportedOperationException("Please implement me");
+        MapDifference<Employee, Integer> difference = Maps.difference(
+            getSalaryMap(oldCTO),
+            getSalaryMap(newCTO)
+        );
+
+        return new Changes(
+            difference.entriesOnlyOnLeft().keySet(),
+            difference.entriesOnlyOnRight().keySet(),
+            difference.entriesDiffering().keySet()
+        );
+    }
+
+    private ImmutableMap<Employee, Integer> getSalaryMap(Employee cto) {
+        return treeTraverserOf(Employee::getSubordinates)
+            .breadthFirstTraversal(cto)
+            .toMap(Employee::getSalary);
+    }
+
+    private static <T> TreeTraverser<T> treeTraverserOf(Function<T, Iterable<T>> childrenFunction) {
+        return new TreeTraverser<T>() {
+            @Override
+            public Iterable<T> children(T root) {
+                return childrenFunction.apply(root);
+            }
+        };
     }
 }
